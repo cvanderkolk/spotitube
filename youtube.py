c41e8d768373ca2f9203a66f1054db3b049c833c
Chris VanderKolk <christopher.n.vanderkolk@gmail.com>
2020-10-20 22:23:53 -0500

    Add basic selection and confirmation screen

--- youtube.py
+++ youtube.py
@@ -9,6 +9,14 @@ def build_youtube_api_v3():
     credentials = build_credentials()
     return googleapiclient.discovery.build('youtube', 'v3', credentials=credentials)
 
+def get_playlists(playlist_id=None):
+    ## TODO: Paginate
+    youtube = build_youtube_api_v3()
+    if playlist_id:
+        return youtube.playlists().list(part='snippet', id=playlist_id).execute()
+    else:
+        return youtube.playlists().list(part='snippet', mine=True, maxResults=50).execute()
+
 def add_video_to_playlist(video_id, playlist_id):
     youtube = build_youtube_api_v3()
     body = {
